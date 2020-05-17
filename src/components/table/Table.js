84045d414454ca4acf7cb7339b21bf1a740dc83b
Mihail Danshin <effectdopler0@gmail.com>
2020-05-17 14:00:27 +0300

    DomListener.js add methods

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,52 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 
-export class Table extends ExcelComponent{
+export class Table extends ExcelComponent {
+  static className = 'excel__table'
 
+  toHTML() {
+    return `
+      <div class="row">
+
+        <div class="row-info"></div>
+
+        <div class="row-data">
+          <div class="column">
+            A
+          </div>
+          <div class="column">
+            B
+          </div>
+          <div class="column">
+            C
+          </div>
+        </div>
+      </div>
+      
+      <div class="row">
+
+        <div class="row-info">
+          1
+        </div>
+
+        <div class="row-data">
+          <div class="cell" contenteditable>1</div>
+          <div class="cell" contenteditable>2</div>
+          <div class="cell" contenteditable>3</div>
+        </div>
+      </div>
+
+      <div class="row">
+
+        <div class="row-info">
+          2
+        </div>
+
+        <div class="row-data">
+          <div class="cell" contenteditable>1</div>
+          <div class="cell" contenteditable>2</div>
+          <div class="cell" contenteditable>3</div>
+        </div>
+      </div>
+    `
+  }
 }
