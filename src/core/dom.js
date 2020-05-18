760956fe5c65d97b356f204fc68caf4723114377
Mikhail Danshin <effectdopler0@gmail.com>
2020-05-18 07:54:49 +0300

    realize removeEventListeners

--- src/core/dom.js
+++ src/core/dom.js
@@ -35,6 +35,10 @@ class Dom {
   on(eventType, callback) {
     this.$el.addEventListener(eventType, callback)
   }
+
+  off(eventType, callback) {
+    this.$el.removeEventListener(eventType, callback)
+  }
 }
 
 export function $(selector) {
