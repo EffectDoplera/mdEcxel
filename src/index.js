bace81253691c82972154fdea5f34d9f7ddde7ae
Mikhail Danshin <effectdopler0@gmail.com>
2020-05-29 13:43:55 +0300

    subscribe on storage

--- src/index.js
+++ src/index.js
@@ -5,9 +5,14 @@ import {Formula} from '@/components/formula/Formula';
 import {Table} from '@/components/table/Table';
 import {createStore} from '@core/createStore'
 import {rootReducer} from '@/redux/rootReducer'
+import {storage} from '@core/utils'
 import './scss/index.scss'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, storage('excel-state'))
+
+store.subscribe(state => {
+  storage('excel-state', state)
+})
 
 const excel = new Excel('#app', {
   components: [Header, Toolbar, Formula, Table],
