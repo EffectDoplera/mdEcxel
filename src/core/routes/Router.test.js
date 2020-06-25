194611e4c601f451f168094ee76a9b00145fa157
Mikhail Danshin <50863514+EffectDoplera@users.noreply.github.com>
2020-06-25 08:03:39 +0300

    Unit testing (#9)
    * setting environment
    
    * create unit testing env

---
+++ src/core/routes/Router.test.js
@@ -0,0 +1,39 @@
+import {Router} from './Router'
+import {Page} from '../Page'
+
+class DashboardPage extends Page {
+  getRoot() {
+    const root = document.createElement('div')
+    root.innerHTML = 'dashboard'
+    return root
+  }
+}
+class ExcelPage extends Page {
+  getRoot() {
+    const root = document.createElement('div')
+    root.innerHTML = 'excel'
+    return root
+  }
+}
+
+describe('Router: ', () => {
+  let router
+  let $root
+
+  beforeEach(() => {
+    $root = document.createElement('div')
+    router = new Router($root, {
+      dashboard: DashboardPage,
+      excel: ExcelPage
+    })
+  })
+
+  test('should be defined', () => {
+    expect(router).toBeDefined()
+  })
+
+  test('should render Dashboard Page', () => {
+    router.changePageHandler()
+    expect($root.innerHTML).toBe('<div>dashboard</div>')
+  })
+})
